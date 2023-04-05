36a54c8b89265faa85ea4dd6d25b12749bda21c4
JunkBezoul <alex.buffet7839@gmail.com>
2023-04-05 13:52:08 +0200

    5

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 console.log("fdhk")
 function getQuote() {
-  fetch("http://localhost:1337/api/citations")
+  fetch("http://localhost:1337/api/citations/?populate=*")
   .then(res => res.json())
   .then(data => {
     let quote = document.querySelector("#quote");
@@ -8,7 +8,7 @@ function getQuote() {
     var i = (Math.floor(Math.random() * data.data.length));
     console.log(data)
   quote.innerHTML = data.data[i].attributes.Citation;
-  author.innerHTML = data.data[i].attributes.Auteur.data[0].attributes.Auteur;
+  author.innerHTML = data.data[i].attributes.Auteurs.data[0].attributes.Auteur;
   })
 }
 
