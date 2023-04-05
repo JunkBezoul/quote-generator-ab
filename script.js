e515f06703ec74789570ef6b4cb9f8be5441a27d
JunkBezoul <alex.buffet7839@gmail.com>
2023-04-05 10:32:50 +0200

    3

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 /*const quote = document.getElementById ("quote");
 const author = document.getElementById ("author");*/
 
-
 console.log("fdhk")
 function getQuote() {
   fetch("http://localhost:1337/api/citations")
-  .then((res) => res.json())
-  .then((data) => {
+  .then(res => res.json())
+  .then(data => {
+    var i = (Math.floor(Math.random() * data.data.length));
     console.log(data)
-  quote.innerHTML = `"${data.data[0].attributes.Citation}"`;
-  author.innerHTML = `- "${data.data[0].attributes.Nom}"`;
+  quote.innerHTML = data.data[i].attributes.Citation;
+  author.innerHTML = data.data[i].attributes.Auteur.data[0].attributes.Auteur;
   })
 }
+
+setInterval(getQuote, 5000);
